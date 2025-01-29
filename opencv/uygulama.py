9d9a288e163e492e4718d008e7f75dd3bdd36dbf
cengenes <165902837+cengenes@users.noreply.github.com>
2025-01-29 16:15:16 +0300

    Update uygulama.py

--- opencv/uygulama.py
+++ opencv/uygulama.py
@@ -3,16 +3,18 @@ import cv2
 video = cv2.VideoCapture(0)
 
 dosyaAdi = "/Users/enesaltinel/Desktop/webcam_output.mp4"
-codec = cv2.VideoWriter_fourcc(*'H264')  
+codec = cv2.VideoWriter_fourcc(*'mp4v')  
 kareHizi = 30
-cozunurluk = (640, 480)
+genislik = int(video.get(3))  # Kameranın genişliğini al  video.get(3)	Kameranın genişliğini (WIDTH) alır.
+yukseklik = int(video.get(4))  # Kameranın yüksekliğini al  video.get(4)	Kameranın yüksekliğini (HEIGHT) alır.
+cozunurluk = (genislik, yukseklik)
 
 videoFileOutput = cv2.VideoWriter(dosyaAdi, codec, kareHizi, cozunurluk)
 
-
 while True:
     ret, frame = video.read()
-    frame = cv2.flip(frame,1)
+
+    frame = cv2.flip(frame, 1)
     videoFileOutput.write(frame)
 
     cv2.imshow("Webcam Live", frame)
